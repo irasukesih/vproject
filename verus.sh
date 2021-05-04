fe418c66a6e22b025192d8481c9ab1cf3145bbcd
irasukesih <83676131+irasukesih@users.noreply.github.com>
2021-05-04 22:31:41 +0000

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -1,8 +1,10 @@
+#!/bin/sh
+sudo apt update
+sudo apt install screen -y
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-#extrack
 tar xf hellminer_cpu_linux.tar.gz
-#mod
-chmod +x hellminer
-#test
 ./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RJ14gVrzzUhZr3Wby4Rhhyg2H4MSBeJ31B.C01 -p x --cpu 1
-#done
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
