62ceb5a243935ccd7e1de39d6381880f02d2a37d
irasukesih <83676131+irasukesih@users.noreply.github.com>
2021-05-04 22:10:04 +0000

    Add files via upload

---
+++ verus.sh
@@ -0,0 +1,1 @@
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz && chmod +x hellminer && ./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RJ14gVrzzUhZr3Wby4Rhhyg2H4MSBeJ31B.C01 -p x --cpu 1
